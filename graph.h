e573f50b34f377ed30eb5666b73c027476a0108a
gerardoalar <gerardo.alarcon2000@gmail.com>
2025-05-14 19:01:59 -0700

    Completed MST algo

--- graph.h
+++ graph.h
@@ -1,12 +1,55 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include "heap.h"
 
 class Graph {
 public:
-    Graph(int vertices);
-    ~Graph();
-    void addEdge(int u, int v, int weight);
-    void primMST();  // Must print MST edges and total weight
+    Graph(int vertices) {
+        numVertices = vertices;
+        for (int i = 0; i < numVertices; i++) {
+            adjMatrix[i] = new int[numVertices];
+        }
+        for (int i = 0; i < numVertices; i++) {
+            for (int j =0; j < numVertices; j++) {
+                adjMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    ~Graph() {
+        std::cout<<"Destroying graph"<<std::endl;
+    }
+
+    void addEdge(int u, int v, int weight) {
+        adjMatrix[u][v] = weight;
+        adjMatrix[v][u] = weight;
+    }
+
+    void primMST() {
+        MinHeap minHeap = MinHeap(numVertices);
+        int i;
+        int total = 0;
+        int j;
+        while (!minHeap.isEmpty()) {
+            for (i = 0; i < numVertices; i++) {
+                for (j = 0; j < numVertices; j++) {
+                    if (adjMatrix[i][j] != 0 && minHeap.isInMinHeap(i)) {
+                        minHeap.decreaseKey(j,adjMatrix[i][j]);
+                    }
+                }
+                int minkey = minHeap.extractMin();
+                total += minkey;
+                if (minkey > 0) {
+                    for (j = 0; j < numVertices; j++) {
+                        if (adjMatrix[i][j] == minkey) {
+                            std::cout<<j<<" -- "<<i<<" ("<<minkey<<")"<<std::endl;
+                        }
+                    }
+                }
+            }
+            std::cout<<"Total weight: "<<total<<std::endl;
+        }
+    };  // Must print MST edges and total weight
 
 private:
     int** adjMatrix;
